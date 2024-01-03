9cca8f0fa82fc90d4e50527976ae60acf2d5caed
Allan Cássio De Matos <153683921+CassioAllan@users.noreply.github.com>
2024-01-03 18:00:26 -0300

    Atualização1.0

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,34 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+data class Usuario(val nome: String)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class ConteudoEducacional(val nome: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarioConteudoPairs: Pair<Usuario, ConteudoEducacional>) {
+        for ((usuario, conteudo) in usuarioConteudoPairs) {
+            if (!inscritos.contains(usuario)) {
+                inscritos.add(usuario)
+                println("Matrícula realizada com sucesso para ${usuario.nome} na $nome no conteúdo ${conteudo.nome}.")
+            } else {
+                println("${usuario.nome} já está matriculado em outro máteria.")
+            }
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Allan")
+    val usuario2 = Usuario("Beatriz")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin")
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin")
+
+    val formacaoKotlin = Formacao("Formação Kotlin", mutableListOf(conteudo1, conteudo2))
+
+    formacaoKotlin.matricular(usuario1 to conteudo1, usuario2 to conteudo2)
+    formacaoKotlin.matricular(usuario1 to conteudo1)
+    
+    formacaoKotlin.conteudos.add(ConteudoEducacional("Desenvolvimento Android em Kotlin"))
 }
